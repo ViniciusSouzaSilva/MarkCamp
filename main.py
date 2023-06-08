607aff661f7d89fa70256e1e747530b04f15c9f2
ViniciusSouzaSilva <vinisic331@gmail.com>
2023-06-08 14:53:19 -0300

    Cadastro de obras
    - Função de cadastro de obras adicionado
    - Cabeçalho dinâmico com informações da obra adicionado

--- main.py
+++ main.py
@@ -2,17 +2,27 @@ from os import system
 from contextlib import closing
 import sqlite3
 
-cab1 = f"""\n{'MARK CAMP':^100}
+cab1 = f"""\n{'MARK CAMP':^120}
 SECRETARIA DE OBRAS PÚBLICAS
 
 LISTA DE OBRAS CADASTRADAS:
 
 {'NÚMERO':<12}{'Nº DE CONTRATAÇÃO':<25}{'NOME':<20}{'ENDEREÇO':<30}"""
+
+cab2 = f"\n{'MARK CAMP':^150}\n\n" + '''OBRA: {a1:<35}GESTOR PUBLICO: {a2:<25}TEL: {a3:<15}EMAIL: {a4:<30}
+ENDEREÇO: {a5:<31}GESTOR PRIVADO: {a6:<25}TEL: {a7:<15}EMAIL: {a8:<30}
+DATA: {a9:<35}PROCESSO DE CONTRATAÇÃO: {a10:<30}
+SITUAÇÃO ATUAL: {a11:25}VALOR ORÇADO: {a12:<30}
+PREVISÃO DE TÉRMINO: {a13:<20}VALOR MEDIDO: {a14:<30}
+'''
+
 opcMainMenu = f"""
 SELECIONE A OPERAÇÃO:
 [1] CADASTRAR OBRA
 [2] VISUALIZAR OBRA
 [3] EXCLUIR OBRA"""
+
+n = 'Lorem ipsum'
 
 obras = []
 
@@ -29,6 +39,7 @@ def iniciarDB():
 def mainMenu():
     system('cls')
     obras.clear()
+
     print(cab1)
 
     with sqlite3.connect('Obras.db') as conexao:
@@ -57,34 +68,78 @@ def mainMenu():
 def cadastrarObra():
     system('cls')
 
+    contratacao = ''
+    obra = ''
+    endereco = ''
+    anoData = ''
+    mesData = ''
+    diaData = ''
+    anoPrev = ''
+    mesPrev = ''
+    diaPrev = ''
+    orcamento = ''
+    situacao = ''
+    data = ''
+    prev = ''
+
+    print(cab2.format(a1=obra, a2='', a3='', a4='', a5=endereco, a6='', a7='', a8='', a9=data, a10=contratacao,a11=situacao, a12=orcamento, a13=prev, a14=''))
     contratacao = input('Número de contratação: ')
+    system('cls')
+
+    print(cab2.format(a1=obra, a2='', a3='', a4='', a5=endereco, a6='', a7='', a8='', a9=data, a10=contratacao, a11=situacao, a12=orcamento, a13=prev, a14=''))
     obra = input('Nome da obra: ')
+    system('cls')
+
+    print(cab2.format(a1=obra, a2='', a3='', a4='', a5=endereco, a6='', a7='', a8='', a9=data, a10=contratacao,a11=situacao, a12=orcamento, a13=prev, a14=''))
     endereco = input('Endereço da obra: ')
+    system('cls')
+
+    print(cab2.format(a1=obra, a2='', a3='', a4='', a5=endereco, a6='', a7='', a8='', a9=data, a10=contratacao,a11=situacao, a12=orcamento, a13=prev, a14=''))
     anoData = int(input('Ano de início: '))
     mesData = int(input('Mês de início: '))
     diaData = int(input('Dia de início: '))
+    data = f"'{anoData}-{mesData}-{diaData}'"
+    system('cls')
+
+    print(cab2.format(a1=obra, a2='', a3='', a4='', a5=endereco, a6='', a7='', a8='', a9=data, a10=contratacao,a11=situacao, a12=orcamento, a13=prev, a14=''))
     anoPrev = int(input('Ano previsto para término: '))
     mesPrev = int(input('Mês previsto para término: '))
     diaPrev = int(input('Dia previsto para término: '))
-    orcamento = int(input('Valor orçado: '))
-    situacao = input('Situação atual: ')
-
-    data = f"'{anoData}-{mesData}-{diaData}'"
     prev = f"'{anoPrev}-{mesPrev}-{diaPrev}'"
+    system('cls')
+
+    print(cab2.format(a1=obra, a2='', a3='', a4='', a5=endereco, a6='', a7='', a8='', a9=data, a10=contratacao,a11=situacao, a12=orcamento, a13=prev, a14=''))
+    orcamento = int(input('Valor orçado: '))
+    system('cls')
+
+    print(cab2.format(a1=obra, a2='', a3='', a4='', a5=endereco, a6='', a7='', a8='', a9=data, a10=contratacao,a11=situacao, a12=orcamento, a13=prev, a14=''))
+    situacao = input('Situação atual: ')
+    system('cls')
 
     with sqlite3.connect('Obras.db') as conexao:
         with closing(conexao.cursor()) as cursor:
             cursor.execute(f'INSERT INTO Obras(Contratacao, Obra, Endereco, Data, Previsao, Situacao, Orcado) VALUES("{contratacao}", "{obra}", "{endereco}", {data}, {prev}, "{situacao}", {orcamento})')
             conexao.commit()
 
+    print(cab2.format(a1=obra, a2='', a3='', a4='', a5=endereco, a6='', a7='', a8='', a9=data, a10=contratacao,a11=situacao, a12=orcamento, a13=prev, a14=''))
+    input('Pressione enter para voltar...')
     mainMenu()
 
 def excluirObra(id):
     with sqlite3.connect('Obras.db') as conexao:
         with closing(conexao.cursor()) as cursor:
-            cursor.execute(f'DELETE FROM Obras WHERE Contratacao = "{obras[id - 1][0]}"' )
+            cursor.execute(f'DELETE FROM Obras WHERE Contratacao = "{obras[id - 1][0]}"')
             conexao.commit()
     mainMenu()
 
 
 iniciarDB()
+
+
+
+
+
+
+
+
+tes = 'Obra: {a:^20} aaaaaaaaa {b}'
